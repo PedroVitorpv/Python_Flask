a00419b22c5d7c83863944f8067e24a800990629
PedroVitorpv <pedrovitorpvcnrn@gmail.com>
2022-08-12 17:54:16 -0300

    Alterei o meu nome no código

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-
-# coding: utf-8
-import os
-os.system('cls')
-
 from flask import Flask, render_template
 app = Flask("projeto")
 
 @app.route ("/")
 def ola_mundo ():
     #criar uma variável com o meu nome
-    nome="Hugo Rodrigues"
+    nome="Pedro Vítor"
     produtos = [
     {"nome": "Ração", "preco": 199.99},
     {"nome": "Playstation", "preco": 1999.99}]
