8d6c17803462d7be159479198e24d41ab2a0b297
PedroVitorpv <pedrovitorpvcnrn@gmail.com>
2022-08-12 17:37:50 -0300

    Atualizei a versão para o professor

---
+++ app.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+import os
+os.system('cls')
+
+from flask import Flask, render_template
+app = Flask("projeto")
+
+@app.route ("/")
+def ola_mundo ():
+    #criar uma variável com o meu nome
+    nome="Hugo Rodrigues"
+    produtos = [
+    {"nome": "Ração", "preco": 199.99},
+    {"nome": "Playstation", "preco": 1999.99}]
+
+    return render_template("alo.html", n=nome, aProdutos=produtos), 200
+
+app.run()
